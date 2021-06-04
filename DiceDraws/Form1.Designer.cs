d1dd6e190c620a594481186c9c36162486c35d0d
Suzie-0820 <85336611+Suzie-0820@users.noreply.github.com>
2021-06-04 12:24:05 +0200

    Add project files.

---
+++ DiceDraws/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace DiceDraws
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnCreateDice = new System.Windows.Forms.Button();
+            this.inputNbrDices = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.invalidInputBox = new System.Windows.Forms.RichTextBox();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnCreateDice
+            // 
+            this.BtnCreateDice.Location = new System.Drawing.Point(336, 69);
+            this.BtnCreateDice.Name = "BtnCreateDice";
+            this.BtnCreateDice.Size = new System.Drawing.Size(129, 41);
+            this.BtnCreateDice.TabIndex = 0;
+            this.BtnCreateDice.Text = "Create Dice(s)";
+            this.BtnCreateDice.UseVisualStyleBackColor = true;
+            this.BtnCreateDice.Click += new System.EventHandler(this.BtnCreateDice_Click);
+            // 
+            // inputNbrDices
+            // 
+            this.inputNbrDices.Location = new System.Drawing.Point(349, 40);
+            this.inputNbrDices.Name = "inputNbrDices";
+            this.inputNbrDices.Size = new System.Drawing.Size(100, 23);
+            this.inputNbrDices.TabIndex = 1;
+            this.inputNbrDices.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "How many dices do you need?";
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.Location = new System.Drawing.Point(143, 278);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(134, 47);
+            this.btnRollDice.TabIndex = 3;
+            this.btnRollDice.Text = "Roll Dice(s)";
+            this.btnRollDice.UseVisualStyleBackColor = true;
+            this.btnRollDice.Visible = false;
+            this.btnRollDice.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(449, 278);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(137, 47);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Visible = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(143, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(286, 126);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(103, 109);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(427, 126);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(109, 109);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(572, 126);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(108, 109);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(349, 126);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(103, 109);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // invalidInputBox
+            // 
+            this.invalidInputBox.Location = new System.Drawing.Point(241, 116);
+            this.invalidInputBox.Name = "invalidInputBox";
+            this.invalidInputBox.Size = new System.Drawing.Size(295, 31);
+            this.invalidInputBox.TabIndex = 10;
+            this.invalidInputBox.Text = "";
+            this.invalidInputBox.Visible = false;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(222, 350);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(326, 52);
+            this.resultBox.TabIndex = 11;
+            this.resultBox.Text = "";
+            this.resultBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.invalidInputBox);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnRollDice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputNbrDices);
+            this.Controls.Add(this.BtnCreateDice);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCreateDice;
+        private System.Windows.Forms.TextBox inputNbrDices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.RichTextBox InvalidInput;
+        private System.Windows.Forms.RichTextBox resultBox;
+        private System.Windows.Forms.RichTextBox invalidInputBox;
+    }
+}
+
---
+++ DiceDraws/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceDraws
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userInput = inputNbrDices.Text;
+            int numberDices = Int32.Parse(userInput);
+            int total = 0; 
+
+            Random diceResult = new Random();
+
+            for (int i = 0; i<numberDices; i++)
+            {
+                int result = diceResult.Next(1, 6);
+                total += result; 
+            }
+
+            resultBox.Visible = true;
+            resultBox.Text = ($"Total of the dices is {total}");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCreateDice_Click(object sender, EventArgs e)
+        {
+            string userInput = inputNbrDices.Text;
+            int number; 
+            bool isNumber = Int32.TryParse(userInput, out number);
+
+           if (isNumber && number > 0 && number <5)
+            {
+                btnQuit.Visible = true;
+                btnRollDice.Visible = true; 
+
+                if (number == 4)
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = true;
+                    pictureBox3.Visible = true;
+                    pictureBox4.Visible = true;
+                    invalidInputBox.Visible = false;
+                }
+
+                if (number == 3)
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox5.Visible = true;
+                    pictureBox4.Visible = true;
+                    invalidInputBox.Visible = false;
+                }
+
+                if (number ==2)
+                {
+                    pictureBox2.Visible = true;
+                    pictureBox3.Visible = true;
+                    invalidInputBox.Visible = false;
+                }
+                
+
+                if (number == 1)
+                {
+                    pictureBox5.Visible = true;
+                    invalidInputBox.Visible = false;
+
+                }
+            }
+           
+           else if (isNumber && number <1 || number >4)
+            {
+                invalidInputBox.Visible = true; 
+                invalidInputBox.Text = "Cannot create less than 1 or more than 4 dices";
+            }
+
+           else if (!isNumber)
+            {
+                invalidInputBox.Visible = true;
+                invalidInputBox.Text = "Please enter a number, between 1 and 4";
+            }
+
+           
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
